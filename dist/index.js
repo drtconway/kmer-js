c91bcc4880422b35142b15d67004ebf1cb336c20
Thomas Conway <drtomc@gmail.com>
2022-06-06 12:24:44 +1000

    add index

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.parseFasta = void 0;
+var FASTA_1 = require("./FASTA");
+Object.defineProperty(exports, "parseFasta", { enumerable: true, get: function () { return FASTA_1.parseFasta; } });
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { parseFasta } from "./FASTA";
